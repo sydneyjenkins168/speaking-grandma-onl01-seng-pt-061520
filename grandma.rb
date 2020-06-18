2c3005ab49b9d094a6764ccf5cb2d31bdb6143c8
Sydney Jenkins <sydjenkins168@gmail.com>
2020-06-18 05:41:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,6 +7,7 @@ def speak_to_grandma(phrase)
   elsif phrase== phrase.upcase
     return "NO, NOT SINCE 1938!"
   end
+  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
