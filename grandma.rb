d79a4992c497f1db1da32e612c697e0be8a4dc7b
Sydney Jenkins <sydjenkins168@gmail.com>
2020-06-18 05:36:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase.upcase
-    return "I LOVE YOU TOO PUMPKIN!"  
+    return "I LOVE YOU TOO PUMPKIN!" 
+  elsif 
   end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
