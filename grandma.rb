cd70c060f72c54348c4987ebda3a6c0d33682772
Sydney Jenkins <sydjenkins168@gmail.com>
2020-06-18 05:42:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,6 +8,8 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   end
   
+  speak_to_grandma("HELLO THERE")
+  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
